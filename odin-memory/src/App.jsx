0a0de685bc626273e98bf6bf8a89f5b0edd1a110
bdaneels <brecht.daneels@hotmail.com>
2024-05-08 16:47:11 +0200

    V1 working

--- odin-memory/src/App.jsx
+++ odin-memory/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import PokemonCard from "./components/pokemon";
+import { v4 as uuid } from "uuid";
 
 const pokemon = [
   "ditto",
@@ -17,15 +17,26 @@ const pokemon = [
   "caterpie",
   "zapdos",
   "moltres",
-  "artequino",
+  "articuno",
   "pikachu",
   "eevee",
   "snorlax",
   "lugia  ",
+  "mewtwo",
+  "mew",
+  "jigglypuff",
+  "psyduck",
+  "meowth",
+  "vulpix",
+  "growlithe",
 ];
 
 function App() {
-  const [pokemon, setPokemon] = useState([]);
+  const [pokemonDB, setPokemonDB] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [selectedPokemons, setSelectedPokemons] = useState([]);
+  const [userSelectedPokemons, setUserSelectedPokemons] = useState([]);
+  const [score, setScore] = useState(0);
 
   useEffect(() => {
     const fetchPokemon = async (pokemonName) => {
@@ -34,19 +45,82 @@ function App() {
           `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
         );
         const data = await response.json();
-        console.log(data);
+        return {
+          name: data.name,
+          id: data.id,
+          image: data.sprites.other["official-artwork"].front_default,
+        };
       } catch (error) {
         console.error("Failed to fetch pokemon:", error);
+        return null;
       }
     };
 
-    // Call fetchPokemon for each item in the pokemon array
-    pokemon.forEach((pokemonName) => {
-      fetchPokemon(pokemonName);
-    });
-  }, [pokemon]); // Include pokemon in the dependency array if it's expected to change
+    Promise.all(pokemon.map((pokemonName) => fetchPokemon(pokemonName))).then(
+      (results) => {
+        const validResults = results.filter(Boolean);
+        setPokemonDB(validResults);
+        const pokemonList = [];
+        while (
+          pokemonList.length < 12 &&
+          validResults.length > pokemonList.length
+        ) {
+          const randomIndex = Math.floor(Math.random() * validResults.length);
+          const randomPokemon = validResults[randomIndex];
+          const randomPokemonAlteredId = { ...randomPokemon, id: uuid() };
+          pokemonList.push(randomPokemonAlteredId);
+        }
+        setSelectedPokemons(pokemonList);
+        setLoading(false);
+      }
+    );
+  }, []);
 
-  return <div>Hello World</div>;
+  function resetSelectedPokemons(pokemon) {
+    const newSelectedPokemons = selectedPokemons.filter(
+      (selectedPokemon) => selectedPokemon.id !== pokemon.id
+    );
+    const randomIndex = Math.floor(Math.random() * pokemonDB.length);
+    const newPokemon = pokemonDB[randomIndex];
+    const newPokemonAlteredId = { ...newPokemon, id: uuid() };
+    setSelectedPokemons([...newSelectedPokemons, newPokemonAlteredId]);
+  }
+
+  function handleClick(pokemon) {
+    console.log("clicked on", pokemon.name);
+    //check if the userselectedpokemons array already has the pokemon
+    if (userSelectedPokemons.includes(pokemon.name)) {
+      //if it does, set the score to 0 and clear the userselectedpokemons array
+      setScore(0);
+      setUserSelectedPokemons([]);
+      console.log("game over");
+      resetSelectedPokemons(pokemon);
+    } else {
+      //if it doesn't, add the pokemon to the array
+      setUserSelectedPokemons([...userSelectedPokemons, pokemon.name]);
+      console.log("userSelectedPokemons", userSelectedPokemons);
+      setScore((prev) => prev + 1);
+      //delete the pokemon of the selected pokemons array and get a new random one from the pokemonDB at the end of the array
+      resetSelectedPokemons(pokemon);
+    }
+  }
+
+  console.log("rendering App component");
+
+  return (
+    <div className="bg-cyan-200 w-full p-10">
+      <span className="text-4xl pb-8"> Score: {score}</span>
+      <div className="grid grid-cols-4 gap-6 pt-6">
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : (
+          selectedPokemons.map((pokemon) => (
+            <PokemonCard pokemon={pokemon} onClick={handleClick} />
+          ))
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- odin-memory/src/components/pokemon.jsx
+++ odin-memory/src/components/pokemon.jsx
@@ -1,3 +1,17 @@
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
+
+function PokemonCard({ pokemon, onClick }) {
+  return (
+    <div
+      className="flex flex-col justify-center items-center border border-black rounded-lg shadow-xl"
+      onClick={() => onClick(pokemon)}
+    >
+      <h2 className="mt-4 text-xl">{pokemon.name}</h2>
+      <img src={pokemon.image} alt={pokemon.name} />
+    </div>
+  );
+}
+
+export default PokemonCard;
